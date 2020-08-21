9c5a3a43f6793f0feef8b46057847b3b25ed117a
RAMIRO EZEQUIEL SANCHEZ LOPEZ <rsanchezlopez@csjn.gov.ar>
2020-08-21 01:10:46 -0300

    cambio webhooks

--- webhook.php
+++ webhook.php
@@ -1,2 +1,16 @@
 <?php
-file_put_contents('output.txt', file_get_contents('php://input'), FILE_APPEND);
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+// Recibir el cuerpo de la petición.
+$input = @file_get_contents("php://input");
+// Parsear el contenido como JSON.
+$eventJson = json_decode($input, true);
+
+// Usar los datos del Webhooks para alguna acción.
+//print $eventJson;
+$json_pretty_string = json_encode($eventJson, JSON_PRETTY_PRINT);
+error_log('test' . PHP_EOL . $json_pretty_string);
+
+// Responder
+http_response_code(200);
+?>
